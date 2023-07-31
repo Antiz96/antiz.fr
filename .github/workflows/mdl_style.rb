abc5774f5719b4ab8aeb3645c5f31fcdfc4490a3
Robin Candau <robincandau@protonmail.com>
2023-07-31 21:55:14 +0200

    Add exception to the mdl test in CI

--- .github/workflows/mdl_style.rb
+++ .github/workflows/mdl_style.rb
@@ -4,7 +4,8 @@ all
 # Exclude the rules that refers to the line length
 exclude_tag :line_length
 
-# Exclude rule MD041 (`hugo` puts metadata at the top of content file)
+# Exclude rules MD002 and MD041 (`hugo` puts metadata at the top of content file which contains the top level title/header)
+exclude_rule 'MD002'
 exclude_rule 'MD041'
 
 # Set accepted trailing spaces to a maximum of 2 (used to break a line) for the MD009 rule
